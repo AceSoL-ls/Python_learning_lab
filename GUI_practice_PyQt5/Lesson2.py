aa9f570fae940d15e8415e100f3fcdf41d805338
AceSoL-ls <eyris97@gmail.com>
2026-04-29 15:57:28 +0300

    Refactored the app through classes

--- GUI_practice_PyQt5/Lesson2.py
+++ GUI_practice_PyQt5/Lesson2.py
@@ -3,80 +3,84 @@ from PyQt5.QtWidgets import (QApplication, QGridLayout,
                              QHBoxLayout, QVBoxLayout,
                              QWidget, QPushButton, QLineEdit)
 
-# App Settings
-app = QApplication([])
-main_window = QWidget()
-main_window.setWindowTitle("Calculator")
-main_window.resize(500,600)
+class CalcApp(QWidget):
+    def __init__(self):
+        super().__init__()
 
-# All Objects
-text_box = QLineEdit()
-grid = QGridLayout()
+        # App Settings
+        self.setWindowTitle("Calculator")
+        self.resize(500, 600)
 
-buttons = [
-    "7","8","9","/",
-    "4","5","6","*",
-    "1","2","3","-",
-    "0",".","=","+"
-]
+        # All Objects
+        self.text_box = QLineEdit()
+        self.grid = QGridLayout()
 
-clear = QPushButton("C")
-delete = QPushButton("<")
+        self.buttons = [
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "0", ".", "=", "+"
+        ]
 
-# Functions
-def button_click():
-    button = app.sender() # <-- What button is clicked
-    text = button.text()  # <-- Based on that button, get the text value from it
-
-    if text == "=":
-        symbol = text_box.text()
-        try:
-            res = eval(symbol)
-            text_box.setText(str(res))
-
-        except Exception as e:
-            print("Error:", e)
-
-    elif text == "C":
-        text_box.clear()
-
-    elif text == "<":
-        current_value = text_box.text()
-        text_box.setText(current_value[:-1])
-
-    else:
-        current_value = text_box.text()
-        text_box.setText(current_value + text)
-
-row = 0
-col = 0
-
-for text in buttons:
-    button = QPushButton(text)
-    button.clicked.connect(button_click)
-    grid.addWidget(button, row, col)
-    col +=1
-
-    if col > 3:
+        row = 0
         col = 0
-        row += 1
+        for text in self.buttons:
+            button = QPushButton(text)
+            button.clicked.connect(self.button_click)
+            self.grid.addWidget(button, row, col)
+            col += 1
 
-# Design
-master_layout = QVBoxLayout()
-master_layout.addWidget(text_box)
-master_layout.addLayout(grid)
+            if col > 3:
+                col = 0
+                row += 1
 
-button_row = QHBoxLayout()
-button_row.addWidget(clear)
-button_row.addWidget(delete)
+        self.clear = QPushButton("C")
+        self.delete = QPushButton("<")
 
-master_layout.addLayout(button_row)
+        # Design
+        master_layout = QVBoxLayout()
+        master_layout.addWidget(self.text_box)
+        master_layout.addLayout(self.grid)
 
-main_window.setLayout(master_layout)
+        button_row = QHBoxLayout()
+        button_row.addWidget(self.clear)
+        button_row.addWidget(self.delete)
 
-clear.clicked.connect(button_click)
-delete.clicked.connect(button_click)
+        master_layout.addLayout(button_row)
 
-# Show/Run
-main_window.show()
-app.exec_()
+        self.setLayout(master_layout)
+
+        self.clear.clicked.connect(self.button_click)
+        self.delete.clicked.connect(self.button_click)
+
+    # Functions
+    def button_click(self):
+
+        button = self.sender()
+        text = button.text()
+
+        if text == "=":
+            symbol = self.text_box.text()
+            try:
+                res = eval(symbol)
+                self.text_box.setText(str(res))
+            except Exception as e:
+                self.text_box.setText("Error")
+                print("Error:", e)
+
+        elif text == "C":
+            self.text_box.clear()
+
+        elif text == "<":
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value[:-1])
+
+        else:
+            current_value = self.text_box.text()
+            self.text_box.setText(current_value + text)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    main_window = CalcApp()
+    main_window.show()
+    app.exec_()
